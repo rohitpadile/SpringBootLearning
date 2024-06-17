a381625270f233d64c118dad241318b99a200c20
Rohit Padile <rohitpadile24@gmail.com>
2024-06-17 19:17:18 +0530

    Comments for Method-level Bean

--- src/main/java/org/gfg/JBDL_71_SrpingBootDemo/DBBean.java
+++ src/main/java/org/gfg/JBDL_71_SrpingBootDemo/DBBean.java
@@ -11,9 +11,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-@Configuration
+@Configuration //Marked with @Configuration, indicating that it is a source of bean definitions.
 public class DBBean {
+//Connection method level bean is created because always we have to open and close connection
+//for a specific method. Instead now by this connection() method we open the connection when we
+//pass it in the constructor of PersonRepository.
+//SO METHOD LEVEL BEAN IS NOT JUST CREATING OBJECT BUT ALSO INVOKING THE METHOD.
 
+//NOTE: Method-level beans are created for the class type that is returned by the method.
+/*
+CONCLUDE:
+When Spring processes the configuration class (`DBBean`), it calls the
+`connection()` method to create a new instance of `Connection`.
+This instance is then stored in the Spring application context as a bean of type `Connection`.
+*/
     @Bean
     public Connection connection() {
         Connection connection = null;
