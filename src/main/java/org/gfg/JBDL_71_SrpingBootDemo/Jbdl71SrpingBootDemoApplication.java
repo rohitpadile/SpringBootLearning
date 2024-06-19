259f37ad9377d8c48d3f06858bc3b6b8a3af0a50
Rohit Padile <rohitpadile24@gmail.com>
2024-06-20 00:32:04 +0530

    minor updates

--- src/main/java/org/gfg/JBDL_71_SrpingBootDemo/Jbdl71SrpingBootDemoApplication.java
+++ src/main/java/org/gfg/JBDL_71_SrpingBootDemo/Jbdl71SrpingBootDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jbdl71SrpingBootDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("ASDFG");
+		System.out.println("Starting Application by Rohit Padile");
 		SpringApplication.run(Jbdl71SrpingBootDemoApplication.class, args);
 	}
 
